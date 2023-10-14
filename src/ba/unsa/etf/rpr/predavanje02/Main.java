66f2cd7976db157184fedc2b6e6f5e5fd05dcb74
vensada <vokanovic@etf.unsa.ba>
2023-10-14 14:49:11 +0200

    promijenjen tekst

--- src/ba/unsa/etf/rpr/predavanje02/Main.java
+++ src/ba/unsa/etf/rpr/predavanje02/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Dobro došli na RPR");
+        System.out.println("Dobro došli na RPR...");
     }
 }
