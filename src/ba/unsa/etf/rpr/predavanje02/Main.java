474224326d79fce30bd93baaf3df7e3c56d27fc8
vensada <vokanovic@etf.unsa.ba>
2023-10-14 14:32:51 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/predavanje02/Main.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.predavanje02;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Dobro došli na RPR");
+    }
+}
